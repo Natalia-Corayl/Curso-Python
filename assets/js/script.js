01877b7d9c60005d7e277a6656e117c0572b31b8
NataliaC <natalia.corayl@gmail.com>
2026-07-02 22:54:21 -0400

    Probando commitear

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+let nombre = prompt("Ingresa tu nombre");
+let apellido = prompt("Ingresa tu apellido");
+let edad = prompt("Ingresa tu edad");
+let deporte_favorito = prompt("Ingresa tu deporte favorito");
+
+
+document.write("Tu nombre es: " + nombre + "<br>");
+document.write("Tu apellido es: " + apellido + "<br>");
+document.write("Tu edad es: " + edad + "<br>");
+document.write("Tu deporte favorito es: " + deporte_favorito + "<br>");
